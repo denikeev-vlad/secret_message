e0eb692ba5864185a09cd53d8eb5452afcc9e30b
admin <vlad-kempo@yandex.ru>
2024-01-21 12:43:46 +0500

    added_onClick_Button_start

--- app/src/main/java/com/example/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/example/secretmessage/WelcomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.secretmessage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
 import com.example.secretmessage.databinding.FragmentWelcomeBinding
 
 
@@ -20,11 +23,15 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+
+        val view = binding.root
+        Log.d("WelcomeFragment", "Фрагмент запустился")
         binding.start.setOnClickListener {
-            
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
         }
 
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return view
     }
 
     override fun onDestroyView() {
